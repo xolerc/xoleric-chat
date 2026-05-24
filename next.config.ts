c2dd54b55f530abf3a80de2275cdc5b7af38a4cd
Xoleric <xoleric@xoleric.uz>
2026-05-24 18:00:21 +0500

    switch to github pages deploy

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-  basePath: "",
+  basePath: "/xoleric-chat",
 };
 
 export default nextConfig;
